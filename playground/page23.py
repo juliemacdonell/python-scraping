907cff124196e3dbbf7eb60c01868336525dec67
Julie MacDonell <julie.macdonell@stylight.com>
2019-04-03 09:20:27 +0200

    Page 23

--- playground/page23.py
+++ playground/page23.py
@@ -4,5 +4,8 @@ from bs4 import BeautifulSoup
 html = urlopen('http://www.pythonscraping.com/pages/page3.html')
 bs = BeautifulSoup(html, 'html.parser')
 
-for child in bs.find('table', {'id':'giftList'}).children:
-    print(child)
+# for child in bs.find('table', {'id':'giftList'}).children:
+#     print(child)
+
+for sibling in bs.find('table', {'id':'giftList'}).tr.next_siblings:
+    print(sibling)
